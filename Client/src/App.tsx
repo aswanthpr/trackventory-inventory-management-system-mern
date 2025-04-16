3f520aad9f16b2614955b039e995b6cd03750aea
aswanthpr <aswanthpr555@gmail.com>
2025-04-16 11:31:59 +0530

    fix:fix some import errors in ui in  component

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,33 +1,4 @@
 
-// import { Route, Routes } from "react-router-dom"
-// import Login from "./pages/Login/Login"
-// import { Layout } from "./pages/Layout/Layout"
-// import InventoryPage from "./pages/Inventory/InventoryMgt"
-// import SalesMgt from "./pages/Sales/SalesMgt"
-// import CustomerMgt from "./pages/customer/CustomerMgt"
-
-// function App() {
-
-
-//   return (
-//     <>
-//       <div className="flex flex-col items-center justify-center min-h-svh">
-//         <Routes>
-//           <Route path="/login" element={<Login />} />
-
-//           <Route path="/" element={<Layout />}>
-//             <Route index element={<InventoryPage />} />
-//             <Route path="/sales-mgt" element={<SalesMgt />} />
-//             <Route path="/customer-mgt" element={<CustomerMgt />} />
-//           </Route>
-//         </Routes>
-//       </div>
-//     </>
-//   )
-// }
-
-// export default App
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Login from "./pages/Login/Login";
 import { Layout } from "./pages/Layout/Layout";
