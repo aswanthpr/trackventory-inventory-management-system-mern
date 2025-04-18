3fea99db430996c676c8dbaad14666fb03238682
aswanthpr <aswanthpr555@gmail.com>
2025-04-18 11:06:52 +0530

    fix:error in util http-error

--- Server/src/utils/http-error.util.ts
+++ Server/src/utils/http-error.util.ts
@@ -1,22 +1,19 @@
-
 export class HttpError extends Error {
-    statusCode: number;
-  
-    constructor(statusCode: number, message: string) {
-      super(message);
-      this.statusCode = statusCode;
-  
-     
-      Object.setPrototypeOf(this, new.target.prototype);
-  
-      
-      if (Error.captureStackTrace) {
-        Error.captureStackTrace(this, this.constructor);
-      }
+  statusCode: number;
+
+  constructor(statusCode: number, message: string) {
+    super(message);
+    this.statusCode = statusCode;
+    this.name = new.target.name;
+
+    Object.setPrototypeOf(this, new.target.prototype);
+
+    if (Error.captureStackTrace) {
+      Error.captureStackTrace(this, this.constructor);
     }
   }
-  
+}
 
 export const createHttpError = (statusCode: number, message: string) => {
-    return new HttpError(statusCode, message);
-}
+  return new HttpError(statusCode, message);
+};
